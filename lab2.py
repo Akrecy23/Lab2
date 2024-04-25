2683d98b20380457963e626df33b2c6a5287eb9a
ak <angelinkje123@gmail.com>
2024-04-25 11:37:26 +0800

    Added average and min+max code

--- lab2.py
+++ lab2.py
@@ -7,10 +7,28 @@ def get_user_input():
     user_list = numbers.split(",")
     print(user_list)
     new_list = [float(i) for i in user_list]
-    print(new_list)
     return new_list
+
+def calc_average(common_list):
+    print(common_list)
+    average = sum(common_list)/len(common_list)
+    print("The average value is " + str(average))
+
+    #Below code shows method 2 of finding sum of list
+    #total = 0
+    #for number in common_list:
+        #total += number
+
+def find_min_max(common_list):
+    min_max_list = [min(common_list),max(common_list)]
+    print("The min value and max value are ")
+    print(min_max_list)
+
 def main():
     display_main_menu()
-    get_user_input()
+    common_list = get_user_input()
+    calc_average(common_list)
+    find_min_max(common_list)
+
 if __name__=='__main__':
     main()
