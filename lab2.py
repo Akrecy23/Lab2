cffdf1ab2f05ff705e3dafa51e71057c6dd2b7fe
ak <angelinkje123@gmail.com>
2024-04-25 10:43:21 +0800

    changed list of strings to floats

--- lab2.py
+++ lab2.py
@@ -3,12 +3,18 @@ def display_main_menu():
     print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
 def get_user_input():
     numbers = input()
-    x = numbers.split(", ")
-    for i in x:
-        i = float(i)
-        #print("Data Type of \"i\" = " + str(type(i)))
-        return i
-#def calc_average():
-    #y = get_user_input()
-    #print ("value returned")
-    #print (y)
+    user_list = numbers.split(",")
+    print(user_list)
+    new_list = [float(i) for i in user_list]
+    print(new_list)
+    return new_list
+def calc_average():
+    y = get_user_input()
+    print ("value returned")
+    print (y)
+def main():
+    display_main_menu()
+    get_user_input()
+    calc_average()
+if __name__=='__main__':
+    main()
