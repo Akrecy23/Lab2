75bf733a20302966bc3137bd7c00e9c5631d2eb9
ak <angelinkje123@gmail.com>
2024-04-25 10:19:05 +0800

    Added code to get user input

--- lab2.py
+++ lab2.py
@@ -1,1 +1,14 @@
 print ("ET0735 (DevOps for AIot) - Lab 2 - Introduction to Python")
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
+def get_user_input():
+    numbers = input()
+    x = numbers.split(", ")
+    for i in x:
+        i = float(i)
+        #print("Data Type of \"i\" = " + str(type(i)))
+        return i
+#def calc_average():
+    #y = get_user_input()
+    #print ("value returned")
+    #print (y)
